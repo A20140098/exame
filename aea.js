c1618197d8ac8206d9a3552e47248120956633a0
A20140098 <131193562+A20140098@users.noreply.github.com>
2023-10-02 12:28:36 -0500

    Add files via upload

---
+++ aea.js
@@ -0,0 +1,37 @@
+var scor = 0;
+alert("Bienvenidos al formulario");
+var pre1 =prompt("¿En que departamento se encuentra el lago titicaca?\n A tacna  \nB Japon \nC ZOe");
+if (pre1 == "A") {
+    scor = scor + V;
+}
+else if(pre1 == "B" || pre1 == "C"){
+    scor = scor (falso);
+}
+else { 
+    alert("Eso no es una respuesta ")
+}
+
+ var pre2 =prompt("¿¿Desde qué parte del Perú se pueden avistar ballenas jorobadas?\n A japon \n B piura y tumbes \n C san miguel");
+if (pre2 == "A") {
+    scor = scor + V;
+}
+else if(pre2 == "B" || pre2 == "C"){
+    scor = scor (falso);
+}
+else { 
+    alert("Eso no es una respuesta ")
+}
+
+
+var pre3 =prompt(" ¿Plato típico de la Selva peruana?\nTacacho con cecina \n ceviche \n pollo broster ");
+if (pre3 == "A") {
+    scor = scor + V;
+}
+else if(pre3 == "B" || pre2 == "C"){
+    scor = scor (falso);
+}
+else { 
+    alert("Eso no es una respuesta ")
+}
+console.log ("Tu scor fue :"+scor);
+document.write ("Tu scor fue :"+scor);
